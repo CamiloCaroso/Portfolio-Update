788dba6cef6e349dbfd828d70a9123a88b12e523
Camilo Caroso <camilitosamuel@hotmail.com>
2019-11-16 14:36:21 -0500

    edit

---
+++ script.js
@@ -0,0 +1,25 @@
+const ps = document.getElementById("ps");
+const cr = document.getElementById("cr");
+const tr = document.getElementById("tr");
+const git = document.getElementById("git");
+const rps = document.getElementById("rps");
+const bj = document.getElementById("bj");
+
+ps.addEventListener('click', function(){
+    window.open("https://camilocaroso.github.io/Psychic-Game/");
+})
+cr.addEventListener('click', function(){
+    window.open("https://camilocaroso.github.io/CrystalsCollector/");
+})
+tr.addEventListener('click', function(){
+    window.open("https://camilocaroso.github.io/TriviaGame/");
+})
+git.addEventListener('click', function(){
+    window.open("https://camilocaroso.github.io/GitTastic/");
+})
+rps.addEventListener('click', function(){
+    window.open(href="https://camilocaroso.github.io/RockPaperScissors/");
+})
+bj.addEventListener('click', function(){
+    window.open("https://camilocaroso.github.io/BlackJack/");
+})
